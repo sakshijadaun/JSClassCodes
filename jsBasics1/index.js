011b6e5a971e04265d3de02b6bd12e6b049112c9
Sakshi Jadaun <sakshijadaun080@gmail.com>
2024-01-14 13:19:23 +0530

    datatypes code add

--- jsBasics1/index.js
+++ jsBasics1/index.js
@@ -4,68 +4,68 @@
  
 //++++++++++++++++++++++ VARIABLE ++++++++++++++++++++++++
 
-let a= 11;
-a=12;    //we can update but not redeclare
-console.log(a);  //op = 12 
+// let a= 11;
+// a=12;    //we can update but not redeclare
+// console.log(a);  //op = 12 
 
-var b=5;
-var b=6; // we can redeclare and update both
-console.log(b);   //op = 6
+// var b=5;
+// var b=6; // we can redeclare and update both
+// console.log(b);   //op = 6
 
-const c=6;
- c=7;  //we can not update neither redeclare
-console.log(c);   //op = 6  
+// const c=6;
+//  c=7;  //we can not update neither redeclare
+// console.log(c);   //op = 6  
 
 
 //++++++++++++++++++++++ DATA TYPE ++++++++++++++++++++++++
 
-// let b;
-// console.log(typeof(b));    OUTPUT -> Undefined
+let b;
+console.log(typeof(b));    OUTPUT -> Undefined
 
-// let a =  b +'2';
-// console.log(b-'2');        OUTPUT = Nan
-// console.log(typeof(a));    OUTPUT = String
+let a =  b +'2';
+console.log(b-'2');        OUTPUT = Nan
+console.log(typeof(a));    OUTPUT = String
 
-// let c= null;
-// console.log(typeof(c));    OUTPUT = object
+let c= null;
+console.log(typeof(c));    OUTPUT = object
 
-// console.log(12-"ab");      OUTPUT = Nan
-// console.log(15+"sakshi");  OUTPUT = 15sakshi
-// console.log(typeof(15+"Sakshi"));     OUTPUT = string
+console.log(12-"ab");      OUTPUT = Nan
+console.log(15+"sakshi");  OUTPUT = 15sakshi
+console.log(typeof(15+"Sakshi"));     OUTPUT = string
 
-// let infinity = 2579251091019159875190571470175185;
-// console.log(infinity);      OUTPUT = 25792510910000000000000000
-// console.log(typeof(infinity));    OUTPUT = number
+let infinity = 2579251091019159875190571470175185;
+console.log(infinity);      OUTPUT = 25792510910000000000000000
+console.log(typeof(infinity));    OUTPUT = number
 
-// let f = 3;
-// let g= 'harsh';
-// let h = 3 - 'harsh';
-// let j = 'jadaun'* 3.8;
-// console.log(f+g);          op = 3harsh
-// console.log(f*g);          op = nan
-// console.log(f/g);          op = nan
-// console.log(f-g);          op = nan
-// console.log(f%g);          op = nan
-// console.log(f**g);         op = nan
-// console.log(j);            op = nan
+let f = 3;
+let g= 'harsh';
+let h = 3 - 'harsh';
+let j = 'jadaun'* 3.8;
+console.log(f+g);          op = 3harsh
+console.log(f*g);          op = nan
+console.log(f/g);          op = nan
+console.log(f-g);          op = nan
+console.log(f%g);          op = nan
+console.log(f**g);         op = nan
+console.log(j);            op = nan
 
-// let i = 'sharma';
-// let h = 3 - 'harsh';
-// console.log(h+i);          op = nansharma
-// console.log(h-i);          op = nan
+let i = 'sharma';
+let h = 3 - 'harsh';
+console.log(h+i);          op = nansharma
+console.log(h-i);          op = nan
 
-// console.log(isNaN(h));         op = true
-// console.log(Number.isNaN(h));  op = true
-// console.log(isNaN(i));         op = true
-// console.log(isNaN(h === j));   op = true (because nan === nan return false isNan(false) then it returns true)
-// console.log(isNaN(h === j));   op = true
-// console.log(isNaN(f));         op = false;
-// console.log(Number.isNaN(f));  op = false;
-// console.log(isNaN(0/0));       op = true;  (0/0 results as nan so output is nan)
-// console.log(isNaN(null));      op = false; (null is treated as a numeric value in this context)
+console.log(isNaN(h));         op = true
+console.log(Number.isNaN(h));  op = true
+console.log(isNaN(i));         op = true
+console.log(isNaN(h === j));   op = true (because nan === nan return false isNan(false) then it returns true)
+console.log(isNaN(h === j));   op = true
+console.log(isNaN(f));         op = false;
+console.log(Number.isNaN(f));  op = false;
+console.log(isNaN(0/0));       op = true;  (0/0 results as nan so output is nan)
+console.log(isNaN(null));      op = false; (null is treated as a numeric value in this context)
 
-// let str = "Shanvi";
-// console.log(str[1]);     op = h
+let str = "Shanvi";
+console.log(str[1]);     op = h
 
 
 //++++++++++++++++++++++ OPERATORS ++++++++++++++++++++++++
